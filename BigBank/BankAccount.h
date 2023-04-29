54afead60229426b44e3d1ab76893aa55b7defc8
David <40316645+twbro54l@users.noreply.github.com>
2023-04-29 19:33:09 +0300

    First upload
    First upload, and last(maybe)

---
+++ BigBank/BankAccount.h
@@ -0,0 +1,18 @@
+#pragma once
+
+using namespace System;
+
+ref class BankAccount abstract {
+public:
+	virtual String^ ToString() override;
+	virtual bool CanDebit(double amount) abstract;
+	BankAccount(String^ holder);
+	void Credit(double amount);
+	bool Debit(double amount);
+	double GetBalance();
+private:
+	String^ accountHolder;
+	double balance;
+protected:
+	String^ RoutingInstructions(double amount);
+};
---
+++ BigBank/SavingsAccount.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "BankAccount.h"
+
+ref class SavingsAccount : BankAccount {
+public:
+	virtual bool CanDebit(double amount) override;
+	SavingsAccount(String^ holder);
+	static void SetInterestRate(double rate);
+	static double GetInterestRate();
+private:
+	static double interestRate;
+};
